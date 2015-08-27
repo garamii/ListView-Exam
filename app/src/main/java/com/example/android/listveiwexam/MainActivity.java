56386e3d128181b9947849c9d2a8cbcfb6f08a27
garam <flowsinyour@naver.com>
2015-08-27 16:15:11 +0900

    두가지 아이템을 표시하는 리스트 뷰 연습

--- app/src/main/java/com/example/android/listveiwexam/MainActivity.java
+++ app/src/main/java/com/example/android/listveiwexam/MainActivity.java
@@ -3,15 +3,17 @@ package com.example.android.listveiwexam;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    private List<String> mData;
-    private ArrayAdapter<String> mAdapter;
+    private List<Map<String, String>> mData;
+    private SimpleAdapter mAdapter;
     private ListView mListView;
 
     @Override
@@ -28,20 +30,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initListView() {
-        mListView =(ListView) findViewById(R.id.listView);
+        mListView = (ListView) findViewById(R.id.listView);
         mListView.setAdapter(mAdapter);
     }
 
     private void initAdapter() {
-        mAdapter = new ArrayAdapter<>(getApplicationContext(),
-                android.R.layout.simple_list_item_1,
-                mData);
+        mAdapter = new SimpleAdapter(getApplicationContext(),
+                mData,
+                android.R.layout.simple_expandable_list_item_2,
+                new String []{"item","subitem"},
+               new int[] {android.R.id.text1,android.R.id.text2});
     }
 
     private void initData() {
         mData = new ArrayList<>();
         for (int i = 1; i <= 100; i++) {
-            mData.add("item" + i);
+            Map<String, String> data = new HashMap<>();
+            data.put("item", "Item" + i);
+            data.put("subitem", "sub Item" + i);
+            mData.add(data);
         }
 
     }
